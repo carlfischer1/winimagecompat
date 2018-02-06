176031ea7720a9481322cc2717294ddb16f5f021
carlfischer1 <carl.fischer@docker.com>
2018-02-06 14:58:45 -0800

    Create getmasterbuild.ps1

---
+++ getmasterbuild.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -UseBasicParsing -OutFile docker.exe https://master.dockerproject.org/windows/x86_64/docker.exe
+Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
