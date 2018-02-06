0db8da76b151c35fcae4c80b01e1f58b646aa53f
carlfischer1 <carl.fischer@docker.com>
2018-02-06 15:00:10 -0800

    Delete getmasterbuild.ps1

--- getmasterbuild.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest -UseBasicParsing -OutFile docker.exe https://master.dockerproject.org/windows/x86_64/docker.exe
-Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
