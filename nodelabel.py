79d584a1ffbb435560561c60b9c2b6e70b6247ca
carlfischer1 <carl.fischer@docker.com>
2018-02-26 11:56:27 -0800

    Update nodelabel.py

--- nodelabel.py
+++ nodelabel.py
@@ -1,33 +1,54 @@
+# nodelabel.py
+# adds a label containing the specified Windows version on Windows nodes in the cluster
+# that do not already have one
+
 import requests
 import json
 
 ucp='https://52.158.239.214'
-winverlabel = 'engine.labels.windowsversion'
-# cert.bundle = cert.pem + key.pem
-# used with cert option to specify client certificate and it's key 
-cert = '/Users/carlfischer/Documents/nodelabel/cert.bundle2'
+winverlabel = 'com.docker.ucp.node.windowsversion'
+headers = {'content-type': 'application/json'}
+params = {}
+body = {}
+ 
+# TODO get from command line input
+winver = '10.0.14393.128'
 
+# certificates and keys from UCP client bundle
+#
+# cert.bundle = cert.pem + key.pem
+# used with requests cert parameter to specify client certificate and it's key 
+cert = '/Users/carlfischer/Documents/nodelabel/cert.bundle2'
+#
 # ca = trusted CA
 # used with verify option to specify the CA as used by the server
 ca = '/Users/carlfischer/Documents/nodelabel/ca.pem'
 
-resp = requests.get(ucp+'/nodes', verify = ca, cert=cert)
-
-# error handling on resp != 200
-
+# get set of nodes from UCP
+resp = requests.get(ucp+'/nodes', verify=ca, cert=cert)
+if resp.status_code != 200:
+    resp.raise_for_status()
+    exit
 nodes = resp.json()
+
+# check each node
+# add label where necessary
 for node in nodes:
     print ('{} {}'.format("Processing node ID: ", node.get('ID')))
-    match = 0
-    # Only Windows nodes
-    if node.get('Description',{}).get('Platform',{}).get('OS') == "windows":
-        for label in node.get('Description',{}).get('Engine',{}).get('Labels',{}):
-            if label == winverlabel:
-                print ("    Existing label found, skipping")
-                match = 1
-            else:
-                print(label)
-        if match == 0:
-            print ("    Setting new label")
-    else:
+    # windows nodes only
+    if node.get('Description',{}).get('Platform',{}).get('OS') != "windows":
         print("    Node OS != Windows, skipping")
+    else:
+       if winverlabel in node.get('Spec',{}).get('Labels',{}):
+            print ("    Existing label found, skipping")
+       else:
+           # new label required
+            print ("    Setting new label")
+            node['Spec']['Labels'][winverlabel] = winver
+            params['version'] = node.get('Version').get('Index')
+            body = json.dumps(node.get('Spec'))
+            # update node in UCP 
+            resp = requests.post(ucp+'/nodes/'+node.get('ID')+'/update', params=params, data=body, headers=headers, verify=ca, cert=cert)
+            if resp.status_code != 200:
+                resp.raise_for_status()
+                exit
