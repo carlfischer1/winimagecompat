a9ab6d7b07310125d69252a9dba53267e513bbc2
carlfischer1 <carl.fischer@docker.com>
2018-02-26 16:10:50 -0800

    add parameter and error handling

--- nodelabel.py
+++ nodelabel.py
@@ -1,28 +1,44 @@
 # nodelabel.py
-# adds a label containing the specified Windows version on Windows nodes in the cluster
-# that do not already have one
+# adds a label with the specified Windows version to Windows nodes in a UCP cluster
+
+# usage: python3 .\nodelabel.py {UCP URL} {Windows version}
+#    ex: python3 .\nodelabel.py https://172.16.4.12 10.0.14393.125
+#
+# covers the following cases
+#   1 - non-Windows nodes (no change)
+#   2 - Windows nodes with an existing label (no change)
+#   3 - preserving existing labels
+#
+# requires
+#   A client bundle downloaded from UCP, with contents unzipped in the working directory
+#   A 'cert.bundle' file created by concatenating cert.pem + key.pem
 
 import requests
 import json
+import os
+import sys
 
-ucp='https://52.158.239.214'
+#ucp = 'https://52.158.239.214'
+clientbundlepath = os.getcwd()
 winverlabel = 'com.docker.ucp.node.windowsversion'
 headers = {'content-type': 'application/json'}
 params = {}
 body = {}
  
-# TODO get from command line input
-winver = '10.0.14393.128'
+# get parameters from command line
+assert (len(sys.argv) == 3), "UCP URL or version argument not specified"
+ucp = sys.argv[1]
+winver = sys.argv[2]
 
 # certificates and keys from UCP client bundle
 #
 # cert.bundle = cert.pem + key.pem
-# used with requests cert parameter to specify client certificate and it's key 
-cert = '/Users/carlfischer/Documents/nodelabel/cert.bundle2'
+#   used with requests cert parameter to specify client certificate and it's key 
+cert = clientbundlepath+'/cert.bundle'
 #
 # ca = trusted CA
-# used with verify option to specify the CA as used by the server
-ca = '/Users/carlfischer/Documents/nodelabel/ca.pem'
+#   used with requests verify parameter to specify the CA used by the server
+ca = clientbundlepath+'/ca.pem'
 
 # get set of nodes from UCP
 resp = requests.get(ucp+'/nodes', verify=ca, cert=cert)
@@ -31,8 +47,7 @@ if resp.status_code != 200:
     exit
 nodes = resp.json()
 
-# check each node
-# add label where necessary
+# check each node and add label where necessary
 for node in nodes:
     print ('{} {}'.format("Processing node ID: ", node.get('ID')))
     # windows nodes only
@@ -45,10 +60,10 @@ for node in nodes:
            # new label required
             print ("    Setting new label")
             node['Spec']['Labels'][winverlabel] = winver
+            data = json.dumps(node.get('Spec'))
             params['version'] = node.get('Version').get('Index')
-            body = json.dumps(node.get('Spec'))
             # update node in UCP 
-            resp = requests.post(ucp+'/nodes/'+node.get('ID')+'/update', params=params, data=body, headers=headers, verify=ca, cert=cert)
+            resp = requests.post(ucp+'/nodes/'+node.get('ID')+'/update', params=params, data=data, headers=headers, verify=ca, cert=cert)
             if resp.status_code != 200:
                 resp.raise_for_status()
                 exit
